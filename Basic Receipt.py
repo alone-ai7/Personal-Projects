4e9593bf68724f9723822043265c44a8b7a413c2
alone-ai7 <elijahtahadlangit7@gmail.com>
2026-08-30 19:55:59 -0700

    Add files via upload

---
+++ Basic Receipt.py
@@ -0,0 +1,30 @@
+store_name = "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tALFAMARI STORE"
+store_loc = "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tIsabang, Lucena, Quezon"
+
+print(store_name, "\n", store_loc)
+
+
+item_name1 = "Water"
+item_price1 = "6.00"
+
+item_name2 = "Biscuit"
+item_price2 = "15.00"
+
+item_name3 = "Toothpaste"
+item_price3 = "50.00"
+
+item_name4 = "Rice"
+item_price4 = "55.00"
+
+item_name5 = "Candy"
+item_price5 = "5.00"
+
+print(item_name1, "=", item_price1)
+print(item_name2, "=", item_price2)
+print(item_name3, "=", item_price3)
+print(item_name4, "=", item_price4)
+print(item_name5, "=", item_price5)
+
+total = float(item_price1) + float(item_price2) + float(item_price3) + float(item_price4) + float(item_price5) 
+
+print("\nThe total price is",total)
